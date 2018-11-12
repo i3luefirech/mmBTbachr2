d309d8468334d6f5d5501951e2c9b4687a7cb8fb
i3luefirech <rbbachmann@bluewin.ch>
2018-11-12 14:57:43 +0100

    add projectfile, add mainsource

---
+++ code/multimouse.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bluefire on 11/12/18.
+//
+
+#include "multimouse.h"
---
+++ code/multimouse.h
@@ -0,0 +1,8 @@
+//
+// Created by bluefire on 11/12/18.
+//
+
+#ifndef CODE_MULTIMOUSE_H
+#define CODE_MULTIMOUSE_H
+
+#endif //CODE_MULTIMOUSE_H
